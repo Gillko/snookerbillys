31c41451bf602e2ea743f8949d9f2863f965bc15
Gilko <gilles_vanpeteghem@hotmail.com>
2017-01-04 11:18:27 +0100

    13th Commit - MatchController and create match view (indetation code and creating a frame when creating a match and adding it to pivot table)

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -7,6 +7,7 @@ use App\Models\Week;
 use App\Models\Season;
 use App\Models\Player;
 use App\Models\Location;
+use App\Models\Frame;
 
 use Illuminate\Http\Request;
 use Input;
@@ -14,154 +15,174 @@ use Validator;
 use Session;
 use Redirect;
 use Response;
+use DB;
 
 class MatchController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        /*Get the matches*/
-        $matches = Match::all();
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		/*Get the matches*/
+		$matches = Match::all();
 
-        /*Load the view and pass the matches*/
-        return \View::make('matches.index')->with('matches', $matches);
-    }
+		/*Load the view and pass the matches*/
+		return \View::make('matches.index')->with('matches', $matches);
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $matches = Match::all();
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		$matches = Match::all();
 
-        $players    = Player::all()->pluck('player_surname', 'player_id');
-        $seasons    = Season::all()->pluck('season_years', 'season_id');
-        $weeks      = Week::all()->pluck('week_name', 'week_id');
-        $locations  = Location::all()->pluck('location_name', 'location_id');
+		$players    = Player::all(DB::raw('concat (player_firstname," ",player_surname) as player_name,player_id'))->pluck('player_name', 'player_id');
+		$seasons    = Season::all()->pluck('season_years', 'season_id');
+		$weeks      = Week::all()->pluck('week_name', 'week_id');
+		$locations  = Location::all()->pluck('location_name', 'location_id');
 
-        return \View::make('matches.create', compact('players', 'weeks', 'seasons', 'locations'));
-    }
+		return \View::make('matches.create', compact('players', 'weeks', 'seasons', 'locations'));
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $input = Input::all();
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$input = Input::all();
 
-        /*Validation*/
-        $rules = array(
-            'match_scorePlayerHome' => 'required',
-            'match_scorePlayerAway' => 'required',
-        );
+		/*Validation*/
+		$rules = array(
+			'match_scorePlayerHome' => 'required',
+			'match_scorePlayerAway' => 'required',
+			'frame_name'            => 'required',
+			'frame_scorePlayerHome' => 'required',
+			'frame_scorePlayerAway' => 'required',
+		);
 
-        /*Run the validation rules on the inputs from the form*/
-        $validator = Validator::make($input, $rules);
+		/*Run the validation rules on the inputs from the form*/
+		$validator = Validator::make($input, $rules);
 
-        if($validator->passes())
-        {
-            /*We make a tutorial object*/
-            $match = new Match();
+		if($validator->passes())
+		{
+			/*We make a tutorial object*/
+			$match  = new Match();
 
-            $match->match_scorePlayerHome       = $input['match_scorePlayerHome'];
-            $match->match_scorePlayerAway       = $input['match_scorePlayerAway'];
-            $match->week_id                     = Input::get('week_id');
-            $match->season_id                   = Input::get('season_id');
-            $match->location_id                 = Input::get('location_id');
-            //$match->user_id    = Auth::id();
+			$match->week_id                     = Input::get('week_id');
+			$match->season_id                   = Input::get('season_id');
+			$match->location_id                 = Input::get('location_id');
+			$match->match_scorePlayerHome       = $input['match_scorePlayerHome'];
+			$match->match_scorePlayerAway       = $input['match_scorePlayerAway'];
+			//$match->user_id    = Auth::id();
 
-            $match->save();
+			$match->save();
 
-            $match->players()->attach($request->input('player_list'));
+			$match->players()->attach($request->input('player_list'));
+			
+			for($i= 0; $i < count($input['frame_name']); $i++){
+				
+				$frames = new Frame();
+				
+				$frames->frame_id               = $request['frame_id'];
+				$frames->frame_name             = $input['frame_name'][$i]; 
+				$frames->frame_scorePlayerHome  = $input['frame_scorePlayerHome'][$i]; 
+				$frames->frame_breakPlayerHome  = $input['frame_breakPlayerHome'][$i];
+				$frames->frame_scorePlayerAway  = $input['frame_scorePlayerAway'][$i];
+				$frames->frame_breakPlayerAway  = $input['frame_breakPlayerAway'][$i];
 
-            /*Redirect*/
-            Session::flash('message', 'Successfully created a match!');
-            return Redirect::to('matches');
-        }
-        else {
-            return Redirect::to('matches/create')->withInput()->withErrors($validator);
-        }
-    }
+				$frames->save();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $match_id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($match_id)
-    {
-        $match = Match::find($match_id);
-        return \View::make('matches.show')->with('match', $match);
-    }
+				$match->frames()->attach($request->get('frame_id', $frames));
+			}
+			
+		   /*Redirect*/
+			Session::flash('message', 'Successfully created a match!');
+			return Redirect::to('matches');
+		}
+		else {
+			return Redirect::to('matches/create')->withInput()->withErrors($validator);
+		}
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $match_id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($match_id)
-    {
-        $match = Match::findOrFail($match_id);
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $match_id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($match_id)
+	{
+		$match = Match::find($match_id);
+		return \View::make('matches.show')->with('match', $match);
+	}
 
-        $players    = Player::all()->pluck('player_surname', 'player_id');
-        $seasons    = Season::all()->pluck('season_years', 'season_id');
-        $weeks      = Week::all()->pluck('week_name', 'week_id');
-        $locations  = Location::all()->pluck('location_name', 'location_id');
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $match_id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($match_id)
+	{
+		$match = Match::findOrFail($match_id);
 
-        return view('matches.edit', compact('match', 'weeks', 'seasons', 'players', 'locations'));
-    }
+		$players    = Player::all()->pluck('player_surname', 'player_id');
+		$seasons    = Season::all()->pluck('season_years', 'season_id');
+		$weeks      = Week::all()->pluck('week_name', 'week_id');
+		$locations  = Location::all()->pluck('location_name', 'location_id');
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $match_id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $match_id)
-    {
-        $match = Match::findOrFail($match_id);
-        $match->update($request->all());
+		return view('matches.edit', compact('match', 'weeks', 'seasons', 'players', 'locations'));
+	}
 
-        $match->players()->sync((array)$request->input('player_list', []));
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $match_id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $match_id)
+	{
+		$match = Match::findOrFail($match_id);
+		$match->update($request->all());
 
-        return redirect('matches');
-    }
+		$match->players()->sync((array)$request->input('player_list', []));
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $match_id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($match_id)
-    {
-        $match = Match::find($match_id);
-        $match->delete();
+		return redirect('matches');
+	}
 
-        /*Redirect*/
-        Session::flash('message', 'Successfully deleted the match!');
-        return Redirect::to('matches');
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $match_id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($match_id)
+	{
+		$match = Match::find($match_id);
+		$match->delete();
 
-     /*JSON action (to use in the Frontoffice with AngularJS*/
-    public function json()
-    {
-        $match = Match::with('players')->with('frames')->get();
+		/*Redirect*/
+		Session::flash('message', 'Successfully deleted the match!');
+		return Redirect::to('matches');
+	}
 
-        //return Response::json($match)->setCallback(Input::get('callback'));
-        //return response()->json($data=['matches' => $match], $status=200, $headers=[], $options=JSON_PRETTY_PRINT);
-        return response()->json($data=$match, $status=200, $headers=[], $options=JSON_PRETTY_PRINT);
-    }
+	 /*JSON action (to use in the Frontoffice with AngularJS*/
+	public function json()
+	{
+		$match = Match::with('players')->with('frames')->get();
+
+		//return Response::json($match)->setCallback(Input::get('callback'));
+		//return response()->json($data=['matches' => $match], $status=200, $headers=[], $options=JSON_PRETTY_PRINT);
+		return response()->json($data=$match, $status=200, $headers=[], $options=JSON_PRETTY_PRINT);
+	}
 }
--- resources/views/matches/create.blade.php
+++ resources/views/matches/create.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.layout')
+@section('head')
+<script src="../assets/js/libraries/jquery/jquery-3.1.1.min.js"></script>
+<script>
+	$(document).ready(function(){
+		$(".addFrame").click(function(){
+			$(".addFields").append(
+				'<label for="frame_name">Name</label><input name="frame_name[]" type="text" id="frame_name"><label for="frame_scorePlayerHome">Score Player Home</label><input name="frame_scorePlayerHome[]" type="text" id="frame_scorePlayerHome"><label for="frame_breakPlayerHome">Break Player Home</label><input name="frame_breakPlayerHome[]" type="text" id="frame_breakPlayerHome"><label for="frame_scorePlayerAway">Score Player Away</label><input name="frame_scorePlayerAway[]" type="text" id="frame_scorePlayerAway"><label for="frame_breakPlayerAway">Break Player Away</label><input name="frame_breakPlayerAway[]" type="text" id="frame_breakPlayerAway">'
+			);
+		});
+	});
+</script>
+@endsection
 @section('content')
 	<div class="container-gv">
 		<div class="content-gv">
@@ -9,11 +21,8 @@
 			</div>
 			<div class="row">
 				<div class="large-12">
-					{{ Form::open(array('url' => 'matches')) }}
-						{{ Form::label('match_scorePlayerHome', 'Score Player Home')}}
-						{{ Form::text('match_scorePlayerHome') }}
-						{{ Form::label('match_scorePlayerAway', 'Score Player Away')}}
-						{{ Form::text('match_scorePlayerAway') }}
+					{{ Form::open(array('url' => 'matches', 'class' => 'formMatch')) }}
+						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 						{{ Form::label('seasons', 'Season')}}
 						{{ Form::select('season_id', $seasons, null) }}
 						{{ Form::label('locations', 'Location')}}
@@ -22,8 +31,28 @@
 						{{ Form::select('week_id', $weeks, null) }}
 						{{ Form::label('player_list', 'Players')}}
 						{{ Form::select('player_list[]', $players, null, ['multiple']) }}
+						{{ Form::label('match_scorePlayerHome', 'Score Player Home')}}
+						{{ Form::text('match_scorePlayerHome') }}
+						{{ Form::label('match_scorePlayerAway', 'Score Player Away')}}
+						{{ Form::text('match_scorePlayerAway') }}
+
+						{{ Form::label('frame_name', 'Name')}}
+						{{ Form::text('frame_name[]') }}
+						{{ Form::label('frame_scorePlayerHome', 'Score Player Home')}}
+						{{ Form::text('frame_scorePlayerHome[]') }}
+						{{ Form::label('frame_breakPlayerHome', 'Break Player Home')}}
+						{{ Form::text('frame_breakPlayerHome[]') }}
+						{{ Form::label('frame_scorePlayerAway', 'Score Player Away')}}
+						{{ Form::text('frame_scorePlayerAway[]') }}
+						{{ Form::label('frame_breakPlayerAway', 'Break Player Away')}}
+						{{ Form::text('frame_breakPlayerAway[]') }}
+
+						{{ Form::button('Add another Frame', ['class' => 'addFrame']) }}
+						<div class="addFields"></div>
 
 						{{ Form::submit('Create the Match!', array('class' => 'button expanded')) }}
+
+						{{-- {{!! Form::hidden('_token','csrf_token()') !!}} --}}
 					{{ Form::close() }}
 				</div>
 			</div>
